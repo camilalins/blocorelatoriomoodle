5a9503dd8e8ddca16f205dcdc31fa2a7eb2f3a87
Camila Lins <gmlins06@gmail.com>
2019-05-02 15:43:41 -0300

    incremento de painel academico troca29

--- painel_academico.php
+++ painel_academico.php
@@ -79,15 +79,105 @@
 			</div>
 		</div>
         <div class="col-md-3 col-sm-6 col-xs-12" style="width: 33%;">
-          <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fas fa-bullhorn" aria-hidden="true"></i></span>
-            <div class="info-box-content">
-              <span class="info-box-number"><a href="pesquisa_satisfacao.php"><small>Pesquisa de Satisfação</small></a>
-            </div>
-          </div>
-        </div>
-    </div>   
+			<div class="info-box">
+				<span class="info-box-icon bg-aqua"><i class="fas fa-bullhorn" aria-hidden="true"></i></span>
+					<div class="info-box-content">
+						<span class="info-box-number"><a href="pesquisa_satisfacao.php"><small>Pesquisa de Satisfação</small></a></span>
+					</div>
+				</div>
+			</div>
+		</div>  
+	</div>
 </section>
+
+<section>
+    <div class="box-header with-border">
+		<h3 class="box-title">Usuários Separados por Cohort</h3>
+	</div>
+	<div class="rows">
+		<div class="col-md-3 col-sm-6 col-xs-12" style="width: 34%;">
+			<div class="info-box">
+				<span class="info-box-icon bg-dodgerblue"><i class="fas fa-ellipsis-v"></i> Usuários Separados por Cohort</span>
+				<div class="info-box-content">
+					<table class="table no-margin">
+						<tbody>
+							<?php
+								require_once("../../config.php");
+								global $DB;
+								$sql5 = "SELECT c.name,count(u.username) AS quantidade ";
+								$sql5 .= "FROM mdl_user u ";
+								$sql5 .= "INNER JOIN mdl_cohort_members cm ON cm.userid=u.id ";
+								$sql5 .= "INNER JOIN mdl_cohort c ON c.id=cm.cohortid ";
+								$sql5 .= "WHERE u.deleted=0 AND u.confirmed=1 ";
+								$sql5 .= "group by c.name ";
+									  
+								$rs5 = (array) $DB->get_records_sql($sql5);
+								//print_r($rs5);
+								if (count($rs5)) 
+								{
+										echo "<thead><tr role=\"row\"><th>Cohort</th><th>Quantidade</th></tr></thead>"; 
+										foreach ($rs5 as $l5) 
+										{
+											echo "<tr class=\"odd\">";
+											echo "<td>" . $l5->name .  "</td><td>" . $l5->quantidade .  "</td>";
+											;
+											echo "</td></tr>";
+										} 
+								};
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12" style="width: 33%;">
+			<div class="info-box">
+				<span class="info-box-icon bg-cornflowerblue"><i class="fas fa-book"></i></span>
+				<div class="info-box-content">
+					<span class="info-box-number"><a href="curso_turma.php"><?php echo $total_curso_ativo->quantidade; ?> <small>Cursos Cadastrados</small></a></span>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12" style="width: 33%;">
+			<div class="info-box">
+				<span class="info-box-icon bg-aqua"><i class="fas fa-bullhorn" aria-hidden="true"></i></span>
+				<div class="info-box-content">
+				  <span class="info-box-number"><a href="pesquisa_satisfacao.php"><small>Pesquisa de Satisfação</small></a></span>
+				</div>
+			</div>	
+		</div>
+	</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <section class="hold-transition skin-blue sidebar-mini">
@@ -347,64 +437,6 @@
     </div>
 </section>
 
-<section class="hold-transition skin-blue sidebar-mini">
-    <div class="box-header with-border">
-		<h3 class="box-title">Usuários Separados por Cohort</h3>
-	</div>
-	<div class="rows">
-		<div class="col-md-3 col-sm-6 col-xs-12" style="width: 34%;">
-			<div class="info-box">
-				<span class="info-box-icon bg-dodgerblue"><i class="fas fa-user-graduate"></i> Usuários Separados por Cohort</span>
-				<div class="info-box-content">
-					<table class="table no-margin">
-						<tbody>
-							<?php
-								require_once("../../config.php");
-								global $DB;
-								$sql5 = "SELECT c.name,count(u.username) AS quantidade ";
-								$sql5 .= "FROM mdl_user u ";
-								$sql5 .= "INNER JOIN mdl_cohort_members cm ON cm.userid=u.id ";
-								$sql5 .= "INNER JOIN mdl_cohort c ON c.id=cm.cohortid ";
-								$sql5 .= "WHERE u.deleted=0 AND u.confirmed=1 ";
-								$sql5 .= "group by c.name ";
-									  
-								$rs5 = (array) $DB->get_records_sql($sql5);
-								//print_r($rs5);
-								if (count($rs5)) 
-									{
-										echo "<thead><tr role=\"row\"><th>Cohort</th><th>Quantidade</th></tr></thead>"; 
-										foreach ($rs5 as $l5) 
-										{
-											echo "<tr class=\"odd\">";
-											echo "<td>" . $l5->name .  "</td><td>" . $l5->quantidade .  "</td>";
-											;
-											echo "</td></tr>";
-										} 
-									};
-							?>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3 col-sm-6 col-xs-12" style="width: 33%;">
-			<div class="info-box">
-				<span class="info-box-icon bg-cornflowerblue"><i class="fas fa-book"></i></span>
-				<div class="info-box-content">
-					<span class="info-box-number"><a href="curso_turma.php"><?php echo $total_curso_ativo->quantidade; ?> <small>Cursos Cadastrados</small></a></span>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3 col-sm-6 col-xs-12" style="width: 33%;">
-			<div class="info-box">
-				<span class="info-box-icon bg-aqua"><i class="fas fa-bullhorn" aria-hidden="true"></i></span>
-				<div class="info-box-content">
-				  <span class="info-box-number"><a href="pesquisa_satisfacao.php"><small>Pesquisa de Satisfação</small></a>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
 
 
 
