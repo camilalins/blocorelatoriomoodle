132d7ecfbc27891cdc51fbe422df1dda68f03037
Camila Lins <gmlins06@gmail.com>
2019-05-03 18:07:53 -0300

    incremento de painel academico troca89

--- cadastro_geral.php
+++ cadastro_geral.php
@@ -6,11 +6,11 @@
   $PAGE->set_url($_SERVER['PHP_SELF']);
   $PAGE->set_pagelayout('admin');
   $PAGE->set_context(context_system::instance());
-  $PAGE->set_url('/blocks/moodleversion/pages/cadastro_geral.php');
-  $PAGE->navbar->add($titulo, new moodle_url("$CFG->httpswwwroot/blocks/moodleversion/pages/cadastro_geral.php"));
+  $PAGE->set_url('/blocks/moodleversion/cadastro_geral.php');
+  $PAGE->navbar->add($titulo, new moodle_url("$CFG->httpswwwroot/blocks/moodleversion/cadastro_geral.php"));
   echo $OUTPUT->header();
 ?>
-<link rel="stylesheet" href="../css/meucss.css">
+<link rel="stylesheet" href="meucss.css">
 <!-- Google Font -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"> 
 <!-- Font Awesome --> 
@@ -34,46 +34,44 @@
             			<small><a class="btn btn-comum" href="exportar_cadastro_geral.php"><i class="fas fa-download"></i> Exportar</a></small>
             			<br>
             			<br>
-            			<h3 class="box-title"><small>Cadastro Geral</small> <?php echo $total_user->quantidade; ?></h3>
+            			<h3 class="box-title"><small>Usuários Cadastrados</small> <?php echo $total_user->quantidade; ?></h3>
           			</div>
           			<div class="box-body">
-            			<div class="row">
+            			<div class="rows">
               				<div class="table-responsive">
             					<table class="table no-margin">
               						<tbody>
-                					<?php
-	                					require_once("../../config.php");
-										global $DB;
-										$sql = "select id, institution, department, quantidade ";
-										$sql .= " from (SELECT id, institution, department, COUNT(institution) AS quantidade";
-										$sql .= " FROM mdl_user rs";
-										$sql .= " WHERE deleted <> 1 and username <> 'guest'";
-										$sql .= " GROUP BY department, institution)x";
-										$sql .= " ORDER BY quantidade DESC";
-										$rs = (array) $DB->get_records_sql($sql);
-									    if (count($rs)) 
-									    {
-										    echo "<thead><tr role=\"row\"><th>Instituição</th><th>Área de Atuação</th><th>Quantidade</th></tr></thead>"; 
-											foreach ($rs as $l) 
+										<?php
+											require_once("../../config.php");
+											global $DB;
+											$sql = "select id, institution, department, quantidade ";
+											$sql .= " from (SELECT id, institution, department, COUNT(institution) AS quantidade";
+											$sql .= " FROM mdl_user rs";
+											$sql .= " WHERE deleted <> 1 and username <> 'guest'";
+											$sql .= " GROUP BY department, institution)x";
+											$sql .= " ORDER BY quantidade DESC";
+											$rs = (array) $DB->get_records_sql($sql);
+											if (count($rs)) 
 											{
-	    										echo "<tr class=\"odd\">";
-	        									echo "<td>" . $l->institution .  "</td><td>" . $l->department .  "</td><td>" . $l->quantidade .  "</td>";
-	        									;
-												echo "</td></tr>";
-											} 
-										};
-									?>
+												echo "<thead><tr role=\"row\"><th>Instituição</th><th>Área de Atuação</th><th>Quantidade</th></tr></thead>"; 
+												foreach ($rs as $l) 
+												{
+													echo "<tr class=\"odd\">";
+													echo "<td>" . $l->institution .  "</td><td>" . $l->department .  "</td><td>" . $l->quantidade .  "</td>";
+													;
+													echo "</td></tr>";
+												} 
+											};
+										?>
 									</tbody>
             					</table>
-            					<div class="box-header with-border">
             				</div>
         				</div>
     				</div>
      			</div>
          	</div>    
         </div>    
-    </div>
-</section>
+	</section>
 
 <?php
 	$PAGE->set_context($context);
