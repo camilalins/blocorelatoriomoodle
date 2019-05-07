493371e675024fad9f613322e69ef0a84ba1e89d
Camila Lins <gmlins06@gmail.com>
2019-05-07 10:47:12 -0300

    incremento de painel academico troca92

--- cadastro_geral.php
+++ cadastro_geral.php
@@ -46,7 +46,7 @@
 										$sql = "select id, institution, department, quantidade ";
 										$sql .= " from (SELECT id, institution, department, COUNT(institution) AS quantidade";
 										$sql .= " FROM mdl_user rs";
-										$sql .= " WHERE deleted <> 1 and username <> 'guest'";
+										$sql .= " WHERE deleted <> 1 and suspended <> 1 and username <> 'guest' and format(username, 0)";
 										$sql .= " GROUP BY department, institution)x";
 										$sql .= " ORDER BY quantidade DESC";
 										$rs = (array) $DB->get_records_sql($sql);
