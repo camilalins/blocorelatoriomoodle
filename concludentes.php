a037c4a4f154d70d8cc9bbe3e5c211c0ffabee00
Camila Lins <gmlins06@gmail.com>
2019-05-15 15:07:47 -0300

    incremento de painel academico troca123

--- concludentes.php
+++ concludentes.php
@@ -2,7 +2,7 @@
 
 	require_once('../../config.php');
 	global $CFG, $DB;
-	$titulo = 'Consulta de Usuário';
+	$titulo = 'Consulta Concludentes';
 
 	$PAGE->set_url($_SERVER['PHP_SELF']);
 	$PAGE->set_pagelayout('admin');
@@ -22,13 +22,42 @@
 
 <section>		
 	<div class="rows">
-		<div class="col-md-12">	
-		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12" style="width: 34%;">
+			<div class="info-box">
+				<span class="info-box-icon bg-dodgerblue"><i class="fas fa-ellipsis-v"></i> Usuários Separados por Grupo</span>
+				<div class="info-box-content">
+					<table class="table no-margin">
+						<tbody>
+							<?php
+									require_once("../../config.php");
+									global $DB;
+									$sql5 = "SELECT c.name,count(u.username) AS quantidade ";
+									$sql5 .= "FROM mdl_user u ";
+									$sql5 .= "INNER JOIN mdl_cohort_members cm ON cm.userid=u.id ";
+									$sql5 .= "INNER JOIN mdl_cohort c ON c.id=cm.cohortid ";
+									$sql5 .= "WHERE u.deleted=0 AND u.confirmed=1 ";
+									$sql5 .= "group by c.name ";
+										  
+									$rs5 = (array) $DB->get_records_sql($sql5);
+									//print_r($rs5);
+									if (count($rs5)) 
+									{
+										echo "<thead><tr role=\"row\"><th>Grupo</th><th>Quantidade</th></tr></thead>"; 
+										foreach ($rs5 as $l5) {
+											echo "<tr class=\"odd\">";
+											echo "<td>" . $l5->name .  "</td><td>" . $l5->quantidade .  "</td>";
+											;
+											echo "</td></tr>";
+										} 
+									};
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>	
 	</div>
 </section>
-
-
-	
 <?php
 	$PAGE->set_context($context);
 	$PAGE->set_pagelayout('incourse');
