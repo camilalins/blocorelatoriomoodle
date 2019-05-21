221e4a95c4c0975a5f85631a4e940bbe9b03bb05
Camila Lins <gmlins06@gmail.com>
2019-05-21 16:07:16 -0300

    incremento de painel academico troca206

--- detalhes_curso.php
+++ detalhes_curso.php
@@ -11,140 +11,19 @@
 	$PAGE->navbar->add($titulo, new moodle_url("$CFG->httpswwwroot/blocks/moodleversion/detalhes_curso.php"));
 	echo $OUTPUT->header();
 ?>
-<h3 class="box-title"><?php echo $titulo; ?></h3>
 <link rel="stylesheet" href="meucss.css">
 <!-- Google Font -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"> 
-
-<section class="hold-transition skin-blue sidebar-mini">
-  <div class="row">
-      <div class="col-md-12">
-        <div class="box">
-          <div class="box-header with-border"><small><a class="btn btn-success btn-sm ad-click-event" href="javascript:history.go(-1)"><i class="fa fa-arrow-left" aria-hidden="true"></i> Voltar</a></small><!--<small> <a class="btn btn-success btn-sm ad-click-event" href="exportar_usuarios_curso.php"><i class="fa fa-download" aria-hidden="true"></i> Exportar</a></small>--></div>
-          <div class="box-body">
-            <div class="row">
-				
-				
-<?php 
-
-require_once("../../config.php");
-
-global $DB;
-
-$sql = "SELECT COUNT(com.id) AS countrecord  ";
-$sql .= " FROM mdl_course_completions com ";
-$sql .= " inner join mdl_course c on c.id = com.course";
-$sql .= " WHERE c.fullname='" . $_REQUEST["escolha_curso"] . "' AND (timecompleted > 0) ";
-//echo $sql;
-
-$disciplina = (array) $DB->get_records_sql($sql);
-foreach($disciplina as $site);
-
-?>
-
-
-<?php 
-
-require_once("../../config.php");
-
-global $DB;
-
-$sql2 = "SELECT COUNT(com.id) AS count2  ";
-$sql2 .= " FROM mdl_course_completions com ";
-$sql2 .= " inner join mdl_course c on c.id = com.course";
-$sql2 .= " WHERE c.fullname='" . $_REQUEST["escolha_curso"] . "' AND (timecompleted IS NULL) AND (reaggregate > 0) ";
-//echo $sql;
-
-$disciplina2 = (array) $DB->get_records_sql($sql2);
-//print_r ($disciplina2);
-foreach($disciplina2 as $site2);
-?>
-
-<?php 
-
-require_once("../../config.php");
-
-global $DB;
-
-$sql3 = "SELECT COUNT(com.id) AS count3  ";
-$sql3 .= " FROM mdl_course_completions com ";
-$sql3 .= " inner join mdl_course c on c.id = com.course";
-$sql3 .= " WHERE c.fullname='" . $_REQUEST["escolha_curso"] . "' AND (timestarted = 0) ";
-//echo $sql;
-
-$disciplina3 = (array) $DB->get_records_sql($sql3);
-//print_r ($disciplina3);
-foreach($disciplina3 as $site3);
-?>
-
-
-<div class="table-responsive"><table  class="table no-margin"><tbody>
-	<thead><tr role="row"><th>Concludentes</th><th>Em Andamento</th><th>Não Iniciou</th></tr></thead>
-	<tr class="odd">
-		<td><?php echo $site->countrecord ; ?></td>
-		<td><?php echo $site2->count2 ; ?></td>
-		<td><?php echo $site3->count3 ; ?></td>
-	</tr>
-
-
-
-<?php 
-
-require_once("../../config.php");
-
-global $DB;
-
-$sql5 = "SELECT gi.id as id, gi.itemname as nome, gi.itemtype as tipo, gi.gradetype as grade, gi.scaleid as escala ";
-$sql5 .= " FROM mdl_grade_items gi ";
-$sql5 .= " right JOIN mdl_course c ON c.id = gi.courseid ";
-$sql5 .= " WHERE c.fullname='" . $_REQUEST["escolha_curso"] . "' ";
-
-$disciplina5 = (array) $DB->get_records_sql($sql5);
-
-echo "<h3 class=\"box-title\" style=\"padding: 0px 0px 0px 17px;\"><i class=\"fa fa-bookmark\" style=\"font-size: 14px; padding: 9px;\" aria-hidden=\"true\"></i>" . $_REQUEST["escolha_curso"] .  "</h3>";
-
-    if (count($disciplina5)) {
-
-		echo "<div class=\"table-responsive\"><table  class=\"table no-margin\"><tbody>"; 
-    	echo "<thead><tr role=\"row\"><th>ID</th><th>Nome da Avaliação</th><th>Tipo de Avaliação</th><th>Módulo	</th><th>Módulo</th></tr></thead>"; 
-
-    foreach ($disciplina5 as $l) {
-    echo "<tr class=\"odd\">";
-        
-        echo "<td>" . $l->id .  "</td><td>" . $l->nome .  "</td><td>" . $l->tipo . "</td><td>" . $l->grade .  "</td><td>" . $l->itemmodule .  "</td>";
-;
- echo "</td></tr>";
-    } 
-
-}else {
-	echo "<div align=center>";
-		
-if ($_REQUEST["escolha_curso"] <> "") {
-	   echo "Não foram encontrados usuários para o curso";
-	   } else {
-		   echo "Insira o nome de usuário desejado";
-	};
-	
-echo "</tbody>";
-}
-echo "</table></div>";
-
-
-?>
-
-
-
-
-
-
-</div></div></div></div></div></section>
-
-
-
-
+<!-- Font Awesome --> 
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+<!-- Total de usuários -->    
 
 <section>
 	<div class="rows">
+		
+		
+		
+		
 		<div class="col-md-3 col-sm-6 col-xs-12" style="width: 33%;">
 				<div class="info-box-topo">
 					<span class="info-box-icon bg-aqua">
@@ -156,6 +35,126 @@ echo "</table></div>";
 						</span>
 					</div>
 				</div>
+				
+				<!--Gráfico 3 CAPACITAÇÃO | SEMIPRESENCIAL-->
+    <?php
+      require_once("../../config.php");
+      global $DB;
+      $sql3 = "SELECT c.fullname AS curso,count(u.id) AS quantidade ";
+      $sql3 .= "FROM mdl_role_assignments rs ";
+      $sql3 .= "INNER JOIN mdl_user u ON u.id=rs.userid ";
+      $sql3 .= "INNER JOIN mdl_context e ON rs.contextid=e.id ";
+      $sql3 .= "INNER JOIN mdl_course c ON c.id=e.instanceid ";
+      $sql3 .= "INNER JOIN mdl_course_categories cate  ON cate.id=c.category ";
+      $sql3 .= "INNER JOIN mdl_groups g ON g.courseid = c.id ";
+      $sql3 .= "WHERE e.contextlevel=50 AND rs.roleid=5 AND cate.path like '/4/12%' AND g.idnumber = ' ' ";
+	  $sql3 .= "group by c.fullname ";
+
+      $rs3 = (array) $DB->get_records_sql($sql3);
+      //print_r($rs);
+    ?>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+    //carregando modulo visualization
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+    //função de monta e desenha o gráfico
+    function drawChart() 
+    {
+      //variavel com armazenamos os dados, um array de array's 
+      //no qual a primeira posição são os nomes das colunas
+      <?php
+        if (count($rs2)) 
+        {
+          echo "var data = google.visualization.arrayToDataTable([\n\r['Curso', 'Quantidade'],"; 
+          foreach ($rs3 as $l3) 
+          {
+          echo "['" . $l3->curso .  "'," . $l3->quantidade .  "],\n\r";
+          } 
+          echo "]);";
+        };
+      ?>
+      //opções para exibição do gráfico
+      var options = 
+      {
+        title: ' ',
+        pieHole: 0.4,
+      };
+      //cria novo objeto PeiChart que recebe 
+      //como parâmetro uma div onde o gráfico será desenhado
+      var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+      //desenha passando os dados e as opções
+          chart.draw(data, options);
+    }
+    //metodo chamado após o carregamento
+    google.setOnLoadCallback(drawChart);
+    </script>
+    <!--fim grafico 3-->  
+	
+	<div class="coluna-grafico">							
+								<div class="grafico3">
+									<div class="description-block border-right border-none">
+										  <?php
+											if (!empty($rs3))
+											{
+											  echo "<ul style=\"list-style:none;\">";
+											  echo "<li id=\"donutchart\" style=\"width: 600px; height: 300px;\"></li>";
+											  echo "</ul>";
+											  echo "<a href=\"grafico_presencial.php\"><span class=\"description-percentage text-green\"><i class=\"fa fa-caret-up\"></i> Veja Mais</span></a>";
+											}
+											else
+											{
+											  echo "<p>Nenhum curso encontrado</p>";
+											}
+										  ?>
+										<h5 class="description-header">Capacitação | Semipresencial</h5>
+									</div>                
+								</div>
+								<div class="grafico4">
+									<div class="description-block border-right border-none">
+										  <?php
+											if (!empty($rs3))
+											{
+											  echo "<ul style=\"list-style:none;\">";
+											  echo "<li id=\"donutchart4\" style=\"width: 600px; height: 300px;\"></li>";
+											  echo "</ul>";
+											  echo "<a href=\"grafico_presencial.php\"><span class=\"description-percentage text-green\"><i class=\"fa fa-caret-up\"></i> Veja Mais</span></a>";
+											}
+											else
+											{
+											  echo "<p>Nenhum curso encontrado</p>";
+											}
+										  ?>
+										<h5 class="description-header">Capacitação | Presencial</h5>
+									</div>                
+								</div>
+							</div>
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
 			</div>
 		</div>
 		
@@ -191,8 +190,6 @@ echo "</table></div>";
 		
 		
 	</div>
-	
-	
 	
 </section>
 
