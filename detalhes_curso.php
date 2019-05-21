e0bcfc738cc2a0074e6dd00d0fcf14a2a79d4ce2
Camila Lins <gmlins06@gmail.com>
2019-05-21 16:55:27 -0300

    incremento de painel academico troca209

--- detalhes_curso.php
+++ detalhes_curso.php
@@ -155,7 +155,7 @@
 									$sql5 .= "INNER JOIN mdl_user_enrolments pre_inscr on pre_inscr.userid = alunos_polo.userid ";
 									$sql5 .= "INNER JOIN mdl_role_assignments inscri on inscri.id = pre_inscr.enrolid ";
 									$sql5 .= "INNER JOIN mdl_user aluno on aluno.id = alunos_polo.userid ";
-									$sql5 .= "WHERE disciplina.id = 12 AND inscri.roleid = 5 ";
+									$sql5 .= "WHERE disciplina.id = '" . $_REQUEST["escolha_curso"] . "' AND inscri.roleid = 5 ";
 									$sql5 .= "group by polo.name ";
 									$sql5 .= "ORDER BY polo.name desc ";
 									
@@ -178,79 +178,9 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-3 col-sm-6 col-xs-12" style="width: 66%;">
-	<div class="info-box">
-		<span class="info-box-icon bg-dodgerblue">
-			<i class="fas fa-chart-bar"></i> Usuários Separados por Grupo
-		</span>
-		<div class="info-box-content">
-			<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-			<script type="text/javascript">
-				google.charts.load('current', {packages: ['corechart', 'bar']});
-				google.charts.setOnLoadCallback(drawBasic);
-
-				function drawBasic() {
-					<?php
-						$rs5 = (array) $DB->get_records_sql($sql5);
-						$color = ['#ff9900','#dc3912','#3366cc','#65b20c','#153268','#c01fe0','#f9140c','#61829d','#8ebbe2','#83c6ff'];
-						$positioncolor = 0;
-						if (count($rs5)) 
-						{
-
-							echo "var data = google.visualization.arrayToDataTable([\n\r['Curso', 'Quantidade', { role: 'style' }],";
-							foreach ($rs5 as $l5) 
-							{
-								echo "['" . $l5->name .  "'," . $l5->quantidade . ",'" . $color[$positioncolor] . "'],\n\r";
-								$positioncolor = $positioncolor + 1;
-							} 
-							echo "]);";
-						};
-					?>
-
-				  var options = {
-					title: ' ',
-					chartArea: {width: '40%'},
-					hAxis: {
-					  title: 'Número de Usuários',
-					  minValue: 0
-					},
-					vAxis: {
-					  title: ' '
-					}
-				  };
-
-				  var chart = new google.visualization.BarChart(document.getElementById('chart_div6'));
-
-				  chart.draw(data, options);
-				}
-			</script>
-			<div class="grafico6">
-				<div class="description-block border-right border-none">
-					<?php
-											if (!empty($rs5))
-											{
-											  echo "<ul style=\"list-style:none;margin:0!important;\">";
-											  echo "<li id=\"chart_div6\"></li>";
-											  echo "</ul>";
-											}
-											else
-											{
-											  echo "<p>Nenhum curso encontrado</p>";
-											}
-					?>				 
-				</div>                
-			</div>						
-	</div>
-	</div>
-</div>
-
-
-
-    <!--fim grafico 6-->  
-			
 			
 		</div>
-	</section>
+</section>
 	
 
 
