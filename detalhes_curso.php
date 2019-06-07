f21f3e9bfd8a03b2e560944f9cede923baab9db9
Camila Lins <gmlins06@gmail.com>
2019-06-07 17:00:17 -0300

    incremento de painel academico 24

--- detalhes_curso.php
+++ detalhes_curso.php
@@ -239,11 +239,11 @@
 							<?php
 								require_once("../../config.php");
 								global $DB;
-								$sql6 = "SELECT cc.id, g.id, g.name AS Turma, COUNT(g.id) as quantidade ";
+								$sql6 = "SELECT cc.id, g.name AS turma, COUNT(g.id) as quantidade ";
 								$sql6 .= "FROM mdl_course_completions cc ";
 								$sql6 .= "INNER JOIN mdl_groups_members gm ON cc.userid = gm.userid ";
-								$sql6 .= "LEFT JOIN mdl_groups g ON gm.groupid = g.id ";
-								$sql6 .= "LEFT JOIN mdl_course c ON g.courseid = c.id ";
+								$sql6 .= "INNER JOIN mdl_groups g ON gm.groupid = g.id ";
+								$sql6 .= "INNER JOIN mdl_course c ON g.courseid = c.id ";
 								$sql6 .= "WHERE c.fullname='" . $_REQUEST["escolha_curso"] . "' AND cc.timecompleted > 0 ";
 								$sql6 .= "group by g.id ";
 										  
