908a78dc4a470bd37fbc03f719c78d07d6eaf995
Camila Lins <gmlins06@gmail.com>
2019-06-07 15:47:57 -0300

    incremento de painel academico 16

--- detalhes_curso.php
+++ detalhes_curso.php
@@ -248,6 +248,7 @@
 								$sql6 .= "group by g.name ";
 										  
 								$rs6 = (array) $DB->get_records_sql($sql6);
+								print_r($rs6);
 								if (count($rs6)) 
 								{
 									echo "<thead><tr role=\"row\"><th>Grupo</th><th>Quantidade</th></tr></thead>"; 
