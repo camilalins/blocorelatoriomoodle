a3c0effde103ec315ea21f2ab3fec4db66d0a3e3
Camila Lins <gmlins06@gmail.com>
2019-06-07 15:55:12 -0300

    incremento de painel academico 17

--- detalhes_curso.php
+++ detalhes_curso.php
@@ -239,12 +239,13 @@
 							<?php
 								require_once("../../config.php");
 								global $DB;
-								$sql6 = "SELECT c.fullname as curso, g.name as turma, COUNT(g.name) as quantidade ";
+								$sql6 = "SELECT g.id, c.fullname as curso, g.name as turma, COUNT(g.name) as quantidade ";
 								$sql6 .= "FROM mdl_groups g ";
 								$sql6 .= "INNER JOIN mdl_course c ON g.courseid = c.id ";
 								$sql6 .= "LEFT JOIN mdl_groups_members gm ON gm.groupid = g.id ";
-								$sql6 .= "LEFT JOIN mdl_course_completions cc ON gm.userid = cc.userid ";
-								$sql6 .= "WHERE cc.timecompleted > 0 AND c.fullname='" . $_REQUEST["escolha_curso"] . "' ";
+								$sql6 .= "LEFT JOIN mdl_course_completions cc ON (gm.userid = cc.userid AND cc.timecompleted > 0) ";
+								$sql6 .= "LEFT JOIN mdl_role_assignments as papel ON (papel.userid = gm.userid) ";
+								$sql6 .= "WHERE c.fullname='" . $_REQUEST["escolha_curso"] . "' AND (papel.roleid = 5 OR papel.roleid IS NULL) ";
 								$sql6 .= "group by g.name ";
 										  
 								$rs6 = (array) $DB->get_records_sql($sql6);
