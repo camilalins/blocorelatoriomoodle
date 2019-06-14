6d1c5e651cfc080dead45a54c0b0d45943090876
Wilson Valente <valentephd@gmail.com>
2019-06-14 11:47:32 -0300

    Alunos concludentes das turmas dos cursos - fix prefix

--- detalhes_curso.php
+++ detalhes_curso.php
@@ -252,10 +252,10 @@
                                 {
                                     foreach ($turmas as $turma) {
                                         $sql116 = "SELECT cc.id, gm.groupid, g.id, g.name AS turma ";
-                                        $sql116 .= "FROM gm35_course_completions cc ";
-                                        $sql116 .= "INNER JOIN gm35_course c ON c.id = cc.course ";
-                                        $sql116 .= "INNER JOIN gm35_groups_members gm ON gm.userid = cc.userid ";
-                                        $sql116 .= "INNER JOIN gm35_groups g ON g.id = gm.groupid ";
+                                        $sql116 .= "FROM mdl_course_completions cc ";
+                                        $sql116 .= "INNER JOIN mdl_course c ON c.id = cc.course ";
+                                        $sql116 .= "INNER JOIN mdl_groups_members gm ON gm.userid = cc.userid ";
+                                        $sql116 .= "INNER JOIN mdl_groups g ON g.id = gm.groupid ";
                                         $sql116 .= "WHERE g.id =" . $turma->id . " AND cc.timecompleted > 0 ";
 
                                         $alunosCompletos = (array) $DB->get_records_sql($sql116);
