96eb4d3061db0da58e84cb138280eb6e043c0a2f
Wilson Valente <valentephd@gmail.com>
2019-06-14 10:41:30 -0300

    Return none alter detalhes_curso

--- detalhes_curso.php
+++ detalhes_curso.php
@@ -240,23 +240,19 @@
                             <?php
                                 require_once("../../config.php");
                                 global $DB;
-                                $sql6 = "SELECT g.id, g.name as turma, COUNT(g.name) as quantidade ";
+                                $sql6 = "SELECT g.id, g.name AS turma ";
                                 $sql6 .= "FROM mdl_groups g ";
                                 $sql6 .= "INNER JOIN mdl_course c ON g.courseid = c.id ";
-                                $sql6 .= "LEFT JOIN mdl_groups_members gm ON gm.groupid = g.id ";
-                                $sql6 .= "LEFT JOIN mdl_course_completions cc ON (gm.userid = cc.userid AND cc.timecompleted > 0) ";
-                                $sql6 .= "LEFT JOIN mdl_role_assignments as papel ON (papel.userid = gm.userid) ";
-                                $sql6 .= "WHERE c.fullname='" . $_REQUEST["escolha_curso"] . "' AND (papel.roleid = 5 OR papel.roleid IS NULL) ";
-                                $sql6 .= "group by g.id ";
+                                $sql6 .= "WHERE c.fullname='" . $_REQUEST["escolha_curso"] . "' ";
 
-                                $rs6 = (array) $DB->get_records_sql($sql6, array $params=null, $limitfrom=0, $limitnum=0));
+                                $rs6 = (array) $DB->get_records_sql($sql6);
                                 print_r($rs6);
                                 if (count($rs6))
                                 {
                                     echo "<thead><tr role=\"row\"><th>Grupo</th><th>Quantidade</th></tr></thead>";
                                     foreach ($rs6 as $l6) {
                                         echo "<tr class=\"odd\">";
-                                        echo "<td>" . $l6->turma .  "</td><td>" . $l6->quantidade .  "</td>";
+                                        echo "<td>" . $l6->turma .  "</td><td> 0 </td>";
                                         ;
                                         echo "</td></tr>";
                                     }
