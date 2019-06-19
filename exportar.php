dafab9d735f252886109f8182fba9a0db2d1ba61
Camila Lins <gmlins06@gmail.com>
2019-06-19 14:29:04 -0300

    incremento de painel academico troca73

--- exportar.php
+++ exportar.php
@@ -5,9 +5,9 @@ require_once("../../config.php");
 
 global $DB;
 
-//header('Content-Type: text/csv; charset=UTF-8');
-//header('Content-Disposition: attachment; filename=Cadastro_Geral(' . date("d-m-y-H-i") . ').csv');
-print_r($_POST);
+header('Content-Type: text/csv; charset=UTF-8');
+header('Content-Disposition: attachment; filename=Cadastro_Geral(' . date("d-m-y-H-i") . ').csv');
+
 $output = fopen('php://output', 'w');
 fputcsv($output, array_map("cvt", array(
     'turma',
@@ -17,7 +17,7 @@ fputcsv($output, array_map("cvt", array(
 	$sql106 = "SELECT g.id, g.name ";
     $sql106 .= "FROM mdl_groups g ";
     $sql106 .= "INNER JOIN mdl_course c ON c.id = g.courseid ";
-    $sql106 .= "WHERE c.fullname='" . $_REQUEST["escolha_curso"] . "' ";
+    $sql106 .= "WHERE c.fullname='" . $_POST["curso"] . "' ";
 
     $turmas = (array) $DB->get_records_sql($sql106);
 	$quantidadeTurmas = count($turmas);
@@ -30,7 +30,7 @@ fputcsv($output, array_map("cvt", array(
                 $sql116 .= "INNER JOIN mdl_user u ON u.id=cc.userid ";
                 $sql116 .= "INNER JOIN mdl_course c ON c.id=cc.course ";
                 $sql116 .= "INNER JOIN mdl_groups_members gm ON ( gm.userid = u.id AND gm.groupid = " . $turma-> id . " ) ";
-                $sql116 .= "WHERE cc.timecompleted > 0 AND c.fullname='" . $_REQUEST["escolha_curso"] . "' ";
+                $sql116 .= "WHERE cc.timecompleted > 0 AND c.fullname='" . $_POST["curso"] . "' ";
 
                 $alunosCompletos = (array) $DB->get_records_sql($sql116);
 
@@ -39,7 +39,7 @@ fputcsv($output, array_map("cvt", array(
                 $sql117 .= "INNER JOIN mdl_user u ON u.id=cc.userid ";
                 $sql117 .= "INNER JOIN mdl_course c ON c.id=cc.course ";
                 $sql117 .= "INNER JOIN mdl_groups_members gm ON ( gm.userid = u.id AND gm.groupid = " . $turma-> id . " ) ";
-                $sql117 .= "WHERE cc.timecompleted IS NULL AND c.fullname='" . $_REQUEST["escolha_curso"] . "' ";
+                $sql117 .= "WHERE cc.timecompleted IS NULL AND c.fullname='" . $_POST["curso"] . "' ";
 
                 $alunosIncompletos = (array) $DB->get_records_sql($sql117);
 
