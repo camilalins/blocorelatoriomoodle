053c608f14f6f40eeb0eedbca81d23a6b60be7f0
Camila Lins <gmlins06@gmail.com>
2019-07-23 15:13:48 -0300

    incremento de painel academico troca47

--- exportar_cadastro_geral.php
+++ exportar_cadastro_geral.php
@@ -1,7 +1,10 @@
 <?php
 
 require_once("../../config.php");
+
+
 global $DB;
+
 header('Content-Type: text/csv; charset=UTF-8');
 header('Content-Disposition: attachment; filename=Cadastro_Geral(' . date("d-m-y-H-i") . ').csv');
 
@@ -12,11 +15,9 @@ fputcsv($output, array_map("cvt", array(
     'Quantidade'
         )), ';');
 $sql = " SELECT id, institution, department, quantidade";
-$sql .= " FROM (SELECT id, institution, department, COUNT(institution) AS quantidade";
-$sql .= " FROM m31_user rs";
-$sql .= " WHERE deleted <> 1 and suspended <> 1 and username <> 'guest' and format(username, 0)";
-$sql .= " GROUP BY department, institution)x";
-$sql .= " ORDER BY quantidade desc";
+$sql .= " FROM mdl_role_assignments ass";
+$sql .= " INNER JOIN mdl_user u ON  u.id = ass.userid";
+$sql .= " WHERE roleid=5 AND deleted <> 1 AND suspended <> 1 AND username <> 'guest'";
 
 $rs = (array) $DB->get_records_sql($sql);
 
