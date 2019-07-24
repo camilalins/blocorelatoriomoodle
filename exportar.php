d383f99d4611f05a29552571e75f5da0e7e71f6e
Wilson Valente <valentephd@gmail.com>
2019-07-24 16:42:03 -0300

    PREFIXO git statusgit status

--- exportar.php
+++ exportar.php
@@ -17,8 +17,8 @@ fputcsv($output, array_map("cvt", array(
 )), ';');
 
 $sql106 = "SELECT g.id, g.name ";
-$sql106 .= "FROM groups g ";
-$sql106 .= "INNER JOIN course c ON c.id = g.courseid ";
+$sql106 .= "FROM mdl_groups g ";
+$sql106 .= "INNER JOIN mdl_course c ON c.id = g.courseid ";
 $sql106 .= "WHERE c.fullname='" . $_POST["curso"] . "' ";
 
 $turmas = (array) $DB->get_records_sql($sql106);
@@ -30,21 +30,21 @@ if ($quantidadeTurmas)
     foreach ($turmas as $turma)
     {
         $sql116 = "SELECT u.id AS id, u.firstname AS nome, u.lastname AS sobrenome, u.phone1 AS matricula, g.name AS turma, (SELECT 'Concludente') AS situacao ";
-        $sql116 .= "FROM course_completions cc ";
-        $sql116 .= "INNER JOIN user u ON u.id=cc.userid ";
-        $sql116 .= "INNER JOIN course c ON c.id=cc.course ";
-        $sql116 .= "INNER JOIN groups_members gm ON ( gm.userid = u.id AND gm.groupid = " . $turma->id . " ) ";
-        $sql116 .= "INNER JOIN groups g ON g.id = gm.groupid ";
+        $sql116 .= "FROM mdl_course_completions cc ";
+        $sql116 .= "INNER JOIN mdl_user u ON u.id=cc.userid ";
+        $sql116 .= "INNER JOIN mdl_course c ON c.id=cc.course ";
+        $sql116 .= "INNER JOIN mdl_groups_members gm ON ( gm.userid = u.id AND gm.groupid = " . $turma->id . " ) ";
+        $sql116 .= "INNER JOIN mdl_groups g ON g.id = gm.groupid ";
         $sql116 .= "WHERE cc.timecompleted > 0 AND c.fullname='" . $_POST["curso"] . "' ";
         
         $alunosCompletos = (array) $DB->get_records_sql($sql116);
 
         $sql117 = "SELECT u.id AS id, u.firstname AS nome, u.lastname AS sobrenome, u.phone1 AS matricula, g.name AS turma, (SELECT 'Não Concludente') AS situacao ";
-        $sql117 .= "FROM course_completions cc ";
-        $sql117 .= "INNER JOIN user u ON u.id=cc.userid ";
-        $sql117 .= "INNER JOIN course c ON c.id=cc.course ";
-        $sql117 .= "INNER JOIN groups_members gm ON ( gm.userid = u.id AND gm.groupid = " . $turma->id . " ) ";
-        $sql117 .= "INNER JOIN groups g ON g.id = gm.groupid ";
+        $sql117 .= "FROM mdl_course_completions cc ";
+        $sql117 .= "INNER JOIN mdl_user u ON u.id=cc.userid ";
+        $sql117 .= "INNER JOIN mdl_course c ON c.id=cc.course ";
+        $sql117 .= "INNER JOIN mdl_groups_members gm ON ( gm.userid = u.id AND gm.groupid = " . $turma->id . " ) ";
+        $sql117 .= "INNER JOIN mdl_groups g ON g.id = gm.groupid ";
         $sql117 .= "WHERE cc.timecompleted IS NULL AND c.fullname='" . $_POST["curso"] . "' ";
 
         $alunosIncompletos = (array) $DB->get_records_sql($sql117);
