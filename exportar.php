0692ac752fc14ab75e18a23623bee38bec07d2f0
Wilson Valente <valentephd@gmail.com>
2019-07-24 16:03:40 -0300

    Acerto do exportar com lista de dados dos alunos

--- exportar.php
+++ exportar.php
@@ -25,31 +25,48 @@ fputcsv($output, array_map("cvt", array(
         {
             foreach ($turmas as $turma)
             {
-                $sql116 = "SELECT COUNT(cc.id) AS quantidade ";
-                $sql116 .= "FROM mdl_course_completions cc ";
-                $sql116 .= "INNER JOIN mdl_user u ON u.id=cc.userid ";
-                $sql116 .= "INNER JOIN mdl_course c ON c.id=cc.course ";
-                $sql116 .= "INNER JOIN mdl_groups_members gm ON ( gm.userid = u.id AND gm.groupid = " . $turma-> id . " ) ";
+                $sql116 = "SELECT u.id AS id, u.firstname AS nome, u.lastname AS sobrenome, u.phone1 AS matricula, g.name AS turma, (SELECT 'Concludente') AS Situação ";
+                $sql116 = "FROM mdl_course_completions cc ";
+                $sql116 = "INNER JOIN mdl_user u ON u.id=cc.userid ";
+                $sql116 = "INNER JOIN mdl_course c ON c.id=cc.course ";
+                $sql116 = "INNER JOIN mdl_groups_members gm ON ( gm.userid = u.id AND gm.groupid = " . $turma-> id . " ) ";
+                $sql116 = "INNER JOIN mdl_groups g ON g.id = gm.groupid ";
                 $sql116 .= "WHERE cc.timecompleted > 0 AND c.fullname='" . $_POST["curso"] . "' ";
 
                 $alunosCompletos = (array) $DB->get_records_sql($sql116);
 
-                $sql117 = "SELECT COUNT(cc.id) AS quantidade ";
-                $sql117 .= "FROM mdl_course_completions cc ";
-                $sql117 .= "INNER JOIN mdl_user u ON u.id=cc.userid ";
-                $sql117 .= "INNER JOIN mdl_course c ON c.id=cc.course ";
-                $sql117 .= "INNER JOIN mdl_groups_members gm ON ( gm.userid = u.id AND gm.groupid = " . $turma-> id . " ) ";
+                $sql117 = "SELECT u.id AS id, u.firstname AS nome, u.lastname AS sobrenome, u.phone1 AS matricula, g.name AS turma, (SELECT 'Não Concludente') AS situacao ";
+                $sql117 = "FROM mdl_course_completions cc ";
+                $sql117 = "INNER JOIN mdl_user u ON u.id=cc.userid ";
+                $sql117 = "INNER JOIN mdl_course c ON c.id=cc.course ";
+                $sql117 = "INNER JOIN mdl_groups_members gm ON ( gm.userid = u.id AND gm.groupid = " . $turma-> id . " ) ";
+                $sql117 = "INNER JOIN mdl_groups g ON g.id = gm.groupid ";
                 $sql117 .= "WHERE cc.timecompleted IS NULL AND c.fullname='" . $_POST["curso"] . "' ";
 
                 $alunosIncompletos = (array) $DB->get_records_sql($sql117);
 
-                                       
-                foreach ($alunosCompletos as $q)
-                    {
-					fputcsv($output, array_map("cvt", array(
-						$turma->name,
-						$q->quantidade
-					)), ';');
+                
+                foreach ($alunosCompletos as $a)
+                {
+                    $nomeCompleto = $a->nome . " " . $a->sobrenome;
+
+                    fputcsv($output, array_map("cvt", array(
+                        $a->id,
+                        $nomeCompleto,
+                        $a->matricula,
+                        $a->turma,
+                        $a->situacao
+                    )), ';');
+                }
+                foreach ($alunosIncompletos as $a)
+                {
+                    fputcsv($output, array_map("cvt", array(
+                        $a->id,
+                        $nomeCompleto,
+                        $a->matricula,
+                        $a->turma,
+                        $a->situacao
+                    )), ';');
 				}
 			}
 		}
